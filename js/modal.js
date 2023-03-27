af96b034680009d243846ee991658f8fcb8bc1d7
TangiLC <tangi@le-cadre.net>
2023-03-27 11:45:37 +0200

    mise à jour searchlist

--- js/modal.js
+++ js/modal.js
@@ -25,6 +25,7 @@ function createList(sua){                  //création de la liste des keywords 
      for (let i = 0; i < listFunnel.length; i++) {      //concat map pour +liste d'objets.key
        tempList=tempList.concat(listFunnel[i].ingredients.map(x => x.ingredient));}
        tempList= verifDouble(tempList,searchWords);
+       tempList= tempList.filter(ingredient => ingredient.toLowerCase().includes(datasearch.value));
     }
     if(sua=='u'){
         for (let i = 0; i < listFunnel.length; i++) {   //concat pour +liste d'items
@@ -46,7 +47,7 @@ function openModal(sua){                               //ouverture de la modale
     let target="";                                     //élément cible du DOM
     if (sua=='s'){
         target=document.querySelector('#ingredients');
-        message="Rechercher un ingrédient";}
+        message="Rechercher :"+datasearch.value;}
     if (sua=='u'){
         target=document.querySelector('#ustensiles');
         message="Ajout Ustensile";}
